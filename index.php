508ab2300e2cbaf911fc186666cbf0988ed22d46
Mawia HL <mawiteascc@gmail.com>
2016-01-29 02:34:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    if(!headers_sent()) {
+        header("Status: 301 Moved Permanently");
+        header(sprintf(
+            'Location: https://%s%s',
+            $_SERVER['HTTP_HOST'],
+            $_SERVER['REQUEST_URI']
+        ));
+        exit();
+    }
+}
+require_once "controller/controller.php";
+$zirna=new Contactbook();
+$zirna->create();
+?>
